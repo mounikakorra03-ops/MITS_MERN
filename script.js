af94162d54e8add12c3caedf87852993ab2181f2
mouni <mounikakorra03@gmail.com>
2026-01-22 16:05:21 +0530

    js Basics and Es6 Feature started

---
+++ script.js
@@ -0,0 +1,195 @@
+//console.log("hello world")
+//var =5;//global scope
+//var a=10;
+//console.log(a);
+
+//let b=20;//block scope
+//console.log(b)
+
+//const c=30;//block scope
+//console.log(c)
+
+//var a=10;//number
+//var str="mouni"//string
+//var bool=true//boolean
+//var un; //undifined
+//var nu=null; //null
+//var big=87453029605455n;
+//var symbol=Symbol("li");
+//console.log(typeof a);
+//console.log(typeof str);
+//console.log(typeof bool);
+//console.log(typeof un);
+//console.log(typeof nu);
+//console.log(typeof big);
+//console.log(typeof symbol);
+
+//Arithamatic operators
+//var a=10;
+//var b="20";
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%b);
+
+//relational operators(<,>,<=,>=,!=,===,!==)
+//var a=10;
+//var b="20";
+//console.log(a>b);//false
+//console.log(a<b);//true
+//console.log(a>=b);//false
+//console.log(a<=b);//true
+//console.log(a==b);//false
+//console.log(a!=b);//true
+//console.log(a===b);//false
+//console.log(a!==b);//true
+
+//Logical operator(&&,||,!)
+//a=true;
+//b=false;
+//console.log(a&&b);
+//console.log(a||b);
+//console.log(!a);
+
+//var arr=[10,20,30,40];
+//console.log(arr);
+//console.log(typeof arr);
+
+//var obj={
+    //name:"mouni",
+    //age:20
+//}
+//console.log(obj);
+//console.log(typeof obj);
+
+//Looking statement
+//for loop
+//for(let i=0;i<=10;i++){
+    //console.log(i);
+//}
+//while loop
+//while(condition){
+//    statement
+//  }
+ //let i=1;
+ //while(i<=10){
+    //console.log(i);
+    //i++;
+ //}
+ //do while
+ //do{
+    // //statement
+ // }while(condition);
+ //i = 1;
+ //do{
+    //console.log('do. .while');
+ //}while(i==0);
+
+ //find even numbers
+ //for(let i=1;i<=10;i++){
+    //if(i%2==0)console.log(i);
+ //}
+
+ //i-9;
+ //if(i%2==0)console.log("even");
+ //else console.log("odd");
+
+ //let mark = 90;
+//if(mark>=90)console.log("O Grade");
+ //else if(mark>=80)console.log("A Grade");
+ //else if(mark>=90)console.log("o Grade");
+ //else console.log("faile");
+
+ // Switch
+
+//let day = 6;
+
+//switch (day) {
+  //case 1:
+    //console.log("Sunday");
+    //break;
+
+  //case 2:
+   // console.log("Monday");
+   // break;
+
+  //case 3:
+    //console.log("Tuesday");
+    //break;
+
+  //case 4:
+    //console.log("Wednesday");
+    //break;
+
+  //case 5:
+    //console.log("Thursday");
+    //break;
+
+  //case 6:
+    //console.log("Friday");
+    //break;
+
+  //case 7:
+    //console.log("Saturday");
+    //break;
+
+  //default:{
+    //console.log("Invalid Input");
+// }
+//}
+
+
+//marks=80;
+//let result=(marks>=90)?"O Grade":(marks>=80)?"A Grade":(marks>=70)?"B Grade":(marks>=0)?"Fail":
+//console.log(result);
+
+
+//add(10,20);
+//var c;
+ //function add(a,b){
+    //c = 10;
+    //console.log(a+b+c);
+ //}
+
+//add(10,20);
+//Arrow function
+//var add=(a,b)=>{
+    //console.log(a+b);
+//}
+//add(10,20);
+
+//console.log(z);
+//var z;
+
+//spread operator(...)
+//var arr1 = [10,20,30];
+//var arr2 = [...arr1,40,50,60];
+//console.log(arr2);
+
+//Destrcturing operator
+//var [m1,m2,m3,m4,m5]=[89,90,99,96,87]
+//console.log(m1);
+//console.log(m2);
+//console.log(m3);
+//console.log(m4);
+//console.log(m5);
+
+
+var {name,age,Dept,marks}={
+    name:"mouni",
+    age:20,
+    Dept:"ds",
+    marks:100
+}
+console.log(name);
+console.log(age);
+console.log(Dept);
+console.log(marks);
+
+
+
+
+
+
+
